07626a7c3c5eadf06dbbf1442451532c87001a44
Nhã Nguyễn Vạn <nha282@gmail.com>
2020-08-07 14:00:38 +0700

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 import torch
 from setuptools import find_packages, setup
-from torch.utils.cpp_extension import CppExtension
-
 
 requirements = ["torch","transformers"]
 
@@ -12,6 +10,5 @@ setup(
     url="https://github.com/bino282/ViNLP",
     description="A NLP toolkit for Vietnamese",
     install_requires=requirements,
-    packages=find_packages(),
-    cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
+    packages=find_packages()
 )
